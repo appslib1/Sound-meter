d6122c26f013b76b96437ebbf9a1c6ee0291455f
appsLib1 <jouad.abderrahim7@gmail.com>
2026-09-23 22:44:47 +0000

    optimising

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.soundmeter.decibel.noisedetector"
         minSdk = 21
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
